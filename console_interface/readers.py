fdb52d188edc15e8d9d1c6cd100ae4e3e377e86e
Adrian Furman <fadikk367@gmail.com>
2021-06-01 23:50:22 +0200

    Add new types of readers and options for directed representations

--- console_interface/readers.py
+++ console_interface/readers.py
@@ -2,7 +2,29 @@ from graphs.Graph import Graph
 
 
 def adjacency_matrix(filename):
-    return Graph.from_file(filename)
+    adj_matrix = []
+
+    with open(filename, 'r') as file:
+        rows = file.read().split('\n')
+
+        for row in rows:
+            if row:
+                adj_matrix.append([int(cell) for cell in row.split(' ')])
+
+    return Graph.from_adjacency_matrix(adj_matrix)
+
+
+def directed_adjacency_matrix(filename):
+    adj_matrix = []
+
+    with open(filename, 'r') as file:
+        rows = file.read().split('\n')
+
+        for row in rows:
+            if row:
+                adj_matrix.append([int(cell) for cell in row.split(' ')])
+
+    return Graph.from_adjacency_matrix(adj_matrix, is_directed=True)
 
 
 def cost_matrix(filename):
@@ -16,6 +38,19 @@ def cost_matrix(filename):
                 matrix.append([int(cell) for cell in row.split(' ')])
 
         return Graph.from_cost_matrix(matrix)
+
+
+def directed_cost_matrix(filename):
+    with open(filename) as f:
+        data = f.read()
+
+        matrix = []
+
+        for row in data.split('\n'):
+            if row:
+                matrix.append([int(cell) for cell in row.split(' ')])
+
+        return Graph.from_cost_matrix(matrix, is_directed=True)
 
 
 def incidence_matrix(filename):
